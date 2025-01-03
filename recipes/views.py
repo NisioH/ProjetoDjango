994f557e764945c8cb1172a81dc7cf410471c4af
nisiohelande <nisiohelandeoliveira@gmail.com>
2025-01-02 22:34:37 -0400

    novos arquivos

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,8 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("HOME")
+    return render(request,"recipes/pages/home.html")
 
-def contato(request):
-    return HttpResponse("CONTATO")
 
-def sobre(request):
-    return HttpResponse("SOBRE")
